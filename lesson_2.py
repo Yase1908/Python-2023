f041ee7a7f5aae9493d59f93194da09dc7c491ea
Yasmine <126566664+Yase1908@users.noreply.github.com>
2023-05-03 22:59:24 +0200

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,7 @@
+"""Define the variables"""
+A = 5
+B = 3
+C = 10
+# Compute and print the logical output
+print(A + B * C < 80) # This should produce True
+print(A / B + C > 5 / 13) # This should produce True
