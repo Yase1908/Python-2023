f8cc57b2a20d5291f4f7b5a4fe0527b78476a258
Yasmine <126566664+Yase1908@users.noreply.github.com>
2023-05-03 21:08:27 +0200

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,5 @@
+dict = [{"brand": "Ford", "model": "Mustang", "year": 1964}, 
+{"brand": "Mercedes", "model": "SL", "year": 1971}, 
+{"brand": "Lamborghini", "model": "Diablo", "year": 1990}]
+
+print(dict)
